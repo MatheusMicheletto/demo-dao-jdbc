5a94344d25cf3955ac57bca4d41191be6d83f20e
Matheus Micheletto <matheus.micheletto@pottencial.com.br>
2022-06-09 17:13:19 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -86,7 +86,7 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setInt(5, obj.getDepartment().getId());
 			st.setInt(6, obj.getId());
 
-			st.executeUpdate();			
+			st.executeUpdate();
 		}
 
 		catch (SQLException e) {
@@ -101,6 +101,25 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
+
+		PreparedStatement st = null;
+		try {
+
+			st = conn.prepareStatement("DELETE FROM seller " + "WHERE Id = ? ");
+
+			st.setInt(1, id);
+			st.executeUpdate();
+
+		}
+
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+
+		finally {
+			DB.closeStatement(st);
+
+		}
 
 	}
 
